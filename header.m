6a95c1c5b66ed3858bfa7a61fc0ac6c9fcf2ce33
jan <jan@janw520.home>
2020-10-27 16:19:32 +0100

    aufgabe 1.1 done

--- header.m
+++ header.m
@@ -1,12 +1,13 @@
 % Aktorik & Sensorik - WS 2020
 %
-% ---Titel---
+% 1.1 Messung des Stillstandsdrehmoment
 %
-% Datum: 27.10.2020
+% Datum:    27.10.2020
 % Autoren:  Anton Kress,    S872899
 %           Jan Abel,       S876662
-%
 
 clear
 home
 close all
+
+
