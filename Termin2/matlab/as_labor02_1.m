70a3fed5d42c299f471b4318fe1331209a16273f
jan <jan@janw520.home>
2020-11-12 23:34:27 +0100

    labor 2.1 angefangen

--- Termin2/matlab/as_labor02_1.m
+++ Termin2/matlab/as_labor02_1.m
@@ -22,45 +22,37 @@ Rs = 1;                             % Messwiderstand      in [V/A]
 delta_t = E(:,2);                   % Phasenverschiebung  in [s]
 f = E(:,1);                         % Frequenzen          in [1/s]
 
-y = tan(2*pi*f.*delta_t);
+% Linearisierte Variante
+y = tan(2*pi*f.*delta_t);           % Y-Achse Faktor - einheitenlos
+
+% lineares Fitting
+f1=polyfit(f, y, 1);
+m=f1(1,1);
+b=f1(1,2);
+
+freq=linspace(500,1500);               % Frequenz            in [1/s]
+
+y1=polyval(f1,freq);
 
 
-% f=linspace(500,1500);
-% delta_t=E(:,2);
-% 
-% x=E(:,1)
-% y=tan(2*pi*x.*delta_t);
-% 
-% 
-% f1=polyfit(x, y, 1)
-% y1=polyval(f1,f);
-% 
-% L = (f1(1,1)*(R+Rs))/(2*pi)
-% OS = f1(1,2)
-% 
-% figure(10000)
-%     plot(x,y, 'o', f,y1,'r','linewidth',2);
-%     grid on;
-%     title('Induktivitaet 2')
-%     xlabel('Frequenz f / Hz')
-%     ylabel('tan(2 pi f d_t)')
-% 
-% L=3.8*10^-4
-%     
-% y2=atan((2*pi*L*f)/(R+Rs))./(2*pi*f);
-%     figure(5);
-%     plot(E(:,1), E(:,2), 'o', f, y2,'r','linewidth',2);
-%     grid;
-%     title('Induktivitaet')
-%     xlabel('Frequenz f / Hz')
-%     ylabel('Zeitverschiebung t / s')
+L = (m*(R+Rs))/(2*pi)
+
+figure(1)
+    plot(f,y, 'o', freq,y1,'r','linewidth',2);
+    grid on;
+    title('Induktivitaet 2')
+    xlabel('Frequenz f / Hz')
+    ylabel('tan(2 pi f d_t)')
     
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-
-
-
-
+    
+    
+y2=atan((2*pi*L*f)/(R+Rs))./(2*pi*f);
+    figure(5);
+    plot(E(:,1), E(:,2), 'o', f, y2,'r','linewidth',2);
+    grid;
+    title('Induktivitaet')
+    xlabel('Frequenz f / Hz')
+    ylabel('Zeitverschiebung t / s')
 
 
 % figure(5);
