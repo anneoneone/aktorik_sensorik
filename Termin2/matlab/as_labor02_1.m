188b90c9d91e273c09bf69faf2bb1700bedf7d86
jan <jan@janw520.home>
2020-11-13 17:18:36 +0100

    labor 2.1 dumdidum

--- Termin2/matlab/as_labor02_1.m
+++ Termin2/matlab/as_labor02_1.m
@@ -22,80 +22,39 @@ Rs = 1;                             % Messwiderstand      in [V/A]
 delta_t = E(:,2);                   % Phasenverschiebung  in [s]
 f = E(:,1);                         % Frequenzen          in [1/s]
 
-% Linearisierte Variante
-y = tan(2*pi*f.*delta_t);           % Y-Achse Faktor - einheitenlos
-
-% lineares Fitting
-f1=polyfit(f, y, 1);
-m=f1(1,1);
-b=f1(1,2);
-
-freq=linspace(500,1500);               % Frequenz            in [1/s]
-
-y1=polyval(f1,freq);
-
-
-L = (m*(R+Rs))/(2*pi)
-
-figure(1)
-    plot(f,y, 'o', freq,y1,'r','linewidth',2);
-    grid on;
-    title('Induktivitaet 2')
-    xlabel('Frequenz f / Hz')
-    ylabel('tan(2 pi f d_t)')
-    
-    
-    
-y2=atan((2*pi*L*f)/(R+Rs))./(2*pi*f);
-    figure(5);
-    plot(E(:,1), E(:,2), 'o', f, y2,'r','linewidth',2);
-    grid;
-    title('Induktivitaet')
-    xlabel('Frequenz f / Hz')
-    ylabel('Zeitverschiebung t / s')
-
-
-% figure(5);
-%     plot(E(:,1), E(:,2), 'o');
+% % Linearisierte Variante
+% y = tan(2*pi*f.*delta_t);           % Y-Achse Faktor - einheitenlos
+% 
+% % lineares Fitting
+% f1=polyfit(f, y, 1);
+% m=f1(1,1);
+% b=f1(1,2);
+% 
+% freq=linspace(500,1500);               % Frequenz            in [1/s]
+% 
+% y1=polyval(f1,freq);
+% 
+% 
+% L = (m*(R+Rs))/(2*pi)
+% 
+% figure(1)
+%     plot(f,y, 'o', freq,y1,'r','linewidth',2);
+%     grid on;
+%     title('Induktivitaet 2')
+%     xlabel('Frequenz f / Hz')
+%     ylabel('tan(2 pi f d_t)')
+%     
+%     
+%     
+% y2=atan((2*pi*L*f)/(R+Rs))./(2*pi*f);
+% 
+%     figure(2);
+%     plot(E(:,1), E(:,2), 'o', f, y2,'r','linewidth',2);
 %     grid;
 %     title('Induktivitaet')
 %     xlabel('Frequenz f / Hz')
 %     ylabel('Zeitverschiebung t / s')
 % 
-% F_import = load('F');
-% R_Shunt = 1;
-% figure(6);
-%     subplot(121)
-%         F = (1 : length(F_import.A))' * F_import.Tinterval;
-%         F(:,2) = F_import.A;
-%         plot(F(:,1), F(:,2), '.', 'Markersize', 1);
-%         grid
-%         title('Sprungantwort');
-%         xlabel('Zeit t / s');
-%         ylabel('Spannung ueber R_shunt U_Rshunt / V');
-%     subplot(122)
-%         F = (1 : length(F_import.A))' * F_import.Tinterval;
-%         F(:,2) = F_import.A;
-%         plot(F(:,1), F_import.B, '.','Markersize', 1);
-%         grid
-%         title('Angelegte Spannung');
-%         xlabel('Zeit t / s');
-%         ylabel('Spannung U / V');
-% 
-% load('Belastet_8V');
-% figure(7);
-%     subplot(211);
-%         plot(Belastet_8V(:,2), Belastet_8V(:,3), 'x');
-%         title('Reibungskonstante (Uo = 8V)');
-%         grid;
-%         xlabel('Strom I / A');
-%         ylabel('INC per T');
-%     subplot(212);
-%         plot(Belastet_8V(:,4), Belastet_8V(:,3), 'x');
-%         grid;
-%         xlabel('Strom I / A');
-%         ylabel('Kraft F / N');
-
 
 
 
