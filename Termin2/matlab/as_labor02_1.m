d22ab0535a17fed38142a9fc8a829e1a4f7f9e64
monodon-monoceros <jan.abel@beuth-hochschule.de>
2020-11-18 17:34:28 +0100

    labor 2.1

--- Termin2/matlab/as_labor02_1.m
+++ Termin2/matlab/as_labor02_1.m
@@ -16,45 +16,44 @@ E = fullfile(FolderName, E_Name);
 load(E);
 
 
-
 R = 3.263586106324851;              % Ankerwiderstand     in [V/A]
 Rs = 1;                             % Messwiderstand      in [V/A]
 delta_t = E(:,2);                   % Phasenverschiebung  in [s]
 f = E(:,1);                         % Frequenzen          in [1/s]
 
-% % Linearisierte Variante
-% y = tan(2*pi*f.*delta_t);           % Y-Achse Faktor - einheitenlos
-% 
-% % lineares Fitting
-% f1=polyfit(f, y, 1);
-% m=f1(1,1);
-% b=f1(1,2);
-% 
-% freq=linspace(500,1500);               % Frequenz            in [1/s]
-% 
-% y1=polyval(f1,freq);
-% 
-% 
-% L = (m*(R+Rs))/(2*pi)
-% 
-% figure(1)
-%     plot(f,y, 'o', freq,y1,'r','linewidth',2);
-%     grid on;
-%     title('Induktivitaet 2')
-%     xlabel('Frequenz f / Hz')
-%     ylabel('tan(2 pi f d_t)')
-%     
-%     
-%     
-% y2=atan((2*pi*L*f)/(R+Rs))./(2*pi*f);
-% 
-%     figure(2);
-%     plot(E(:,1), E(:,2), 'o', f, y2,'r','linewidth',2);
-%     grid;
-%     title('Induktivitaet')
-%     xlabel('Frequenz f / Hz')
-%     ylabel('Zeitverschiebung t / s')
-% 
+% Linearisierte Variante
+y = tan(2*pi*f.*delta_t);           % Y-Achse Faktor - einheitenlos
+
+% lineares Fitting
+f1=polyfit(f, y, 1);
+m=f1(1,1);
+b=f1(1,2);
+
+freq=linspace(500,1500);               % Frequenz            in [1/s]
+
+y1=polyval(f1,freq);
+
+
+L = (m*(R+Rs))/(2*pi)
+
+figure(1)
+    plot(f,y, 'o', freq,y1,'r','linewidth',2);
+    grid on;
+    title('Induktivitaet 2')
+    xlabel('Frequenz f / Hz')
+    ylabel('tan(2 pi f d_t)')
+    
+    
+    
+y2=atan((2*pi*L*f)/(R+Rs))./(2*pi*f);
+
+    figure(2);
+    plot(E(:,1), E(:,2), 'o', f, y2,'r','linewidth',2);
+    grid;
+    title('Induktivitaet')
+    xlabel('Frequenz f / Hz')
+    ylabel('Zeitverschiebung t / s')
+
 
 
 
